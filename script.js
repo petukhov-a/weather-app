fa373123202cf29cc2853b1a6132b772bf8869b7
Anton <reopik@mail.ru>
2023-06-25 21:33:48 +0300

    created and made work input for location

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const currentTemperature = document.querySelector('.current-temperature');
 const currentLocation = document.querySelector('.current-location');
 const weatherIconImg = document.querySelector('.weather-icon-img');
+const searchCityInput = document.querySelector('.search-city-input');
+const searchCityForm = document.querySelector('.search-city-form');
 
-const url = 'http://api.weatherapi.com/v1/current.json?key=f307338ed98942f6a28203223232406&q=Moscow&aqi=no';
-fetch(url, {
-    method: 'GET'
-})
-  .then(response => response.json())
-  .then(data => {
-    currentLocation.textContent = data.location.name;
-    currentTemperature.textContent = data.current.temp_c + '°C';
-    weatherIconImg.setAttribute('src', data.current.condition.icon);
-  });
+const api = 'f307338ed98942f6a28203223232406';
+
+const fetchWeather = (url) => {
+    fetch(url, {
+        method: 'GET'
+    })
+      .then(response => response.json())
+      .then(data => {
+        currentLocation.textContent = data.location.name;
+        currentTemperature.textContent = data.current.temp_c + '°C';
+        weatherIconImg.setAttribute('src', data.current.condition.icon);
+      });
+}
+
+let locationName = 'Moscow';
+const url = `http://api.weatherapi.com/v1/current.json?key=${api}&q=${locationName}&aqi=no`;
+
+fetchWeather(url);
+
+searchCityForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    locationName = searchCityInput.value;
+    const url = `http://api.weatherapi.com/v1/current.json?key=${api}&q=${locationName}&aqi=no`;
+    fetchWeather(url);
+});
