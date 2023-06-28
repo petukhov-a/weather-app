3b7a794f531c61c3ffade38268e98bf7c665afbe
Anton <reopik@mail.ru>
2023-06-28 18:11:59 +0300

    changed structure of hours and days forecast wrapper, added headings

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const searchCityInput = document.querySelector('.search-city-input');
 const searchLocationForm = document.querySelector('.search-location-form');
 const refreshWeatherBtn = document.querySelector('.refresh-weather-btn');
 const refreshIcon = refreshWeatherBtn.querySelector('i');
-const forecastHourWrapper = document.querySelector('.forecast-hour-wrapper');
-const forecast3DaysWrapper = document.querySelector('.forecast-3-days-wrapper');
+const forecastForHours = document.querySelector('.forecast-for-hours');
+const forecast3Days = document.querySelector('.forecast-3-days');
 
 const monthsNames = {
     '1': 'January',
@@ -41,49 +41,46 @@ const showCurrentWeather = (data) => {
     currentWeatherIconImg.setAttribute('src', data.current.condition.icon);
 }
 
-const showPerHourWeather = (data) => {
-    forecastHourWrapper.innerHTML = '';
+const showWeatherPerHour = (data) => {
+    forecastForHours.innerHTML = '';
     const forecastHours = data.forecast.forecastday[0].hour;
 
     forecastHours.forEach(forecastHour => {
         const forecastForHourElement = document.createElement('div');
+        forecastForHourElement.classList.add('forecast-for-hour');
         const hour = forecastHour.time.split(' ').at(-1);
         let temperature = formatTemperature(forecastHour.temp_c);
 
         forecastForHourElement.innerHTML = `
-            <div class="forecast-for-hour">
-                <img src=${forecastHour.condition.icon} class="forecast-icon-img">
-                <p class="forecast-hour-temperature">${temperature}</p>
-                <p class="hour">${hour}</p>
-            </div>
+            <img src=${forecastHour.condition.icon} class="forecast-icon-img">
+            <p class="forecast-hour-temperature">${temperature}</p>
+            <p class="hour">${hour}</p>
             `
-        forecastHourWrapper.append(forecastForHourElement);
+        forecastForHours.append(forecastForHourElement);
     })
 }
 
 showWeatherForDays = (data) => {
-    forecast3DaysWrapper.innerHTML = `
-    `;
+    forecast3Days.innerHTML = ``;
 
     data.forecast.forecastday.forEach(forecastDay => {
-        const forecastForDay = document.createElement('div');
+        const forecastForDaysElement = document.createElement('div');
+        forecastForDaysElement.classList.add('forecast-for-day');
         let [year, month, day] = forecastDay.date.split('-');
     
         const minTemperature = formatTemperature(forecastDay.day.mintemp_c);
         const maxTemperature = formatTemperature(forecastDay.day.maxtemp_c);
     
-        forecastForDay.innerHTML = `
-            <div class="forecast-for-day">
-                <p class="forecast-date">${day} ${monthsNames[+month]}</p>
-                <img src="${forecastDay.day.condition.icon}" class="forecast-day-icon-img">
-                <p class="min-max-temperature">
-                    <span class="min">${minTemperature}</span>
-                    <span class="max">${maxTemperature}</span>
-                </p>
-            </div>
+        forecastForDaysElement.innerHTML = `
+            <p class="forecast-date">${day} ${monthsNames[+month]}</p>
+            <img src="${forecastDay.day.condition.icon}" class="forecast-day-icon-img">
+            <p class="min-max-temperature">
+                <span class="min">${minTemperature}</span>
+                <span class="max">${maxTemperature}</span>
+            </p>
         `
     
-        forecast3DaysWrapper.append(forecastForDay);
+        forecast3Days.append(forecastForDaysElement);
     });
 }
 
@@ -96,7 +93,7 @@ const fetchWeather = (url) => {
       .then(data => {
         if (data.error === undefined) {
             showCurrentWeather(data);
-            showPerHourWeather(data);
+            showWeatherPerHour(data);
             showWeatherForDays(data);
         }
     },)
@@ -108,7 +105,7 @@ fetchWeather(url);
 searchLocationForm.addEventListener('submit', (e) => {
     e.preventDefault();
     locationName = searchCityInput.value;
-    url = `http://api.weatherapi.com/v1/forecast.json?key=${api}&q=${locationName}&days=3&aqi=no&alerts=no`;;
+    url = `http://api.weatherapi.com/v1/forecast.json?key=${api}&q=${locationName}&days=3&aqi=no&alerts=no`;
     searchCityInput.value = '';
     fetchWeather(url);
 });
