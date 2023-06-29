977e50ef9500454c189ce9060f5a02601d36e396
Anton <reopik@mail.ru>
2023-06-29 21:06:41 +0300

    add showing more current weather information by clicking on current weather

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const currentTemperature = document.querySelector('.current-temperature');
 const currentLocation = document.querySelector('.current-location');
+const currentWeatherWrapper = document.querySelector('.current-weather-wrapper');
 const currentWeatherIconImg = document.querySelector('.current-weather-icon-img');
 const lastUpdated = document.querySelector('.last-updated');
 const searchCityInput = document.querySelector('.search-city-input');
 const searchLocationForm = document.querySelector('.search-location-form');
 const refreshWeatherBtn = document.querySelector('.refresh-weather-btn');
 const refreshIcon = refreshWeatherBtn.querySelector('i');
+const forecastHourWrapper = document.querySelector('.forecast-hour-wrapper');
+const forecast3DaysWrapper = document.querySelector('.forecast-3-days-wrapper');
 const forecastForHours = document.querySelector('.forecast-for-hours');
 const forecast3Days = document.querySelector('.forecast-3-days');
+const currentWindSpeed = document.querySelector('.wind-speed');
+const currentRainChance = document.querySelector('.rain-chance');
+const feelsLike = document.querySelector('.feels-like');
+const currentVisibilityKm = document.querySelector('.visibility-km');
 
 const monthsNames = {
     '1': 'January',
@@ -33,12 +40,20 @@ const formatTemperature = (temperature) => {
 }
 
 const showCurrentWeather = (data) => {
-    let temperature = formatTemperature(data.current.temp_c);
+    const temperature = formatTemperature(data.current.temp_c);
+    const feelsLikeTemperature = formatTemperature(data.current.feelslike_c);
+    const windSpeed = data.current.wind_kph;
+    const visibilityKm = data.current.vis_km;
+    const rainChance = data.forecast.forecastday[0].day.daily_chance_of_rain;
 
-    lastUpdated.innerHTML = `<span>Last updated</span> ${data.current.last_updated}`;
+    lastUpdated.innerHTML = `<span>Last updated</span><br> ${data.current.last_updated}`;
     currentLocation.textContent = data.location.name;
     currentTemperature.textContent = temperature;
     currentWeatherIconImg.setAttribute('src', data.current.condition.icon);
+    feelsLike.innerHTML = `<span>Feels like</span> ${feelsLikeTemperature}`;
+    currentWindSpeed.innerHTML = `<span>Wind speed</span> ${windSpeed}kph`;
+    currentVisibilityKm.innerHTML = `<span>Visibility</span> ${visibilityKm}km`;
+    currentRainChance.innerHTML = `<span>Rain chance</span> ${rainChance}%`;
 }
 
 const showWeatherPerHour = (data) => {
@@ -61,7 +76,7 @@ const showWeatherPerHour = (data) => {
 }
 
 showWeatherForDays = (data) => {
-    forecast3Days.innerHTML = ``;
+    forecast3Days.innerHTML = '';
 
     data.forecast.forecastday.forEach(forecastDay => {
         const forecastForDaysElement = document.createElement('div');
@@ -74,10 +89,10 @@ showWeatherForDays = (data) => {
         forecastForDaysElement.innerHTML = `
             <p class="forecast-date">${day} ${monthsNames[+month]}</p>
             <img src="${forecastDay.day.condition.icon}" class="forecast-day-icon-img">
-            <p class="min-max-temperature">
-                <span class="min">${minTemperature}</span>
-                <span class="max">${maxTemperature}</span>
-            </p>
+            <div class="min-max-temperature">
+                <p class="min">${minTemperature}</p>
+                <p class="max">${maxTemperature}</p>
+            </div>
         `
     
         forecast3Days.append(forecastForDaysElement);
@@ -112,4 +127,15 @@ searchLocationForm.addEventListener('submit', (e) => {
 
 refreshWeatherBtn.addEventListener('click', () => {
     fetchWeather(url);
+});
+
+currentWeatherWrapper.addEventListener('click', () => {
+    const currentWeatherLeft = document.querySelector('.current-weather-left');
+    const currentWeatherRight = document.querySelector('.current-weather-right');
+
+    forecast3DaysWrapper.classList.toggle('active-3-days-wrapper');
+    forecastHourWrapper.classList.toggle('active-hour-wrapper');
+
+    currentWeatherLeft.classList.toggle('active-current-weather-sides');
+    currentWeatherRight.classList.toggle('active-current-weather-sides');
 });
